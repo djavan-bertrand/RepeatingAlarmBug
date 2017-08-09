c1551e3002723d113f0e04475d5a5625a445a333
Djavan Bertrand <sousoum.prod@gmail.com>
2017-08-09 21:45:14 +0200

    [DEV] Added Reschedule Exact Allow when Idle Elapsed impl.
    The exact alarm is set each time the notification is posted.
    
    alarmManager.setExactAndAllowWhileIdle(
                   AlarmManager.ELAPSED_REALTIME_WAKEUP,
                   SystemClock.elapsedRealtime() + repeatTime, pending);

--- app/src/main/java/sousoum/com/repeatingalarmbug/AlarmScheduler.java
+++ app/src/main/java/sousoum/com/repeatingalarmbug/AlarmScheduler.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.util.Log;
@@ -26,9 +27,13 @@ class AlarmScheduler {
         Log.d("AlarmBug", "Cancel the pending intent");
         alarmManager.cancel(pending);
 
-        Log.d("AlarmBug", "Set inexact repeating in elapsed + 1 minute");
-        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                SystemClock.elapsedRealtime() + repeatTime, repeatTime, pending);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Log.d("AlarmBug", "Set exact and allow while idle in elapsed + 1 minute");
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    SystemClock.elapsedRealtime() + repeatTime, pending);
+        } else {
+            Log.e("AlarmBug", "API 23 is needed to test this implementation.");
+        }
     }
 
 }
